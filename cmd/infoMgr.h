d272b9577e0001c1b5a7440e4ee7f4a4a28e0df0
harryyyhuang <47514466+harryyyhuang@users.noreply.github.com>
2020-06-20 17:31:33 +0800

    Add files via upload

--- cmd/infoMgr.h
+++ cmd/infoMgr.h
@@ -29,19 +29,48 @@ public:
 
 
 
-    //write the storage file
+    // write the storage file
     void writeUserTxt();
     void writeObjectTxt();
+
+//--------------------------------------------------------------------------------------------------------
+//  functions for system to call //
+
+    // register functions
+    bool setID(string UserID) // return false if same id exist
+    void setPassWord(string Password) 
+
+    // borrow functions
+    bool borrowByID(string id); // return false if object has been borrowed or not exist
+    bool borrowByName(string objName);// return false if there's no available object or not exist
+    
+    //return functions
+    bool returnByID(string id); // return false if id not exist or haven't borrowed
+    bool returnByName(string objName); // return false if haven't borrowed
+//---------------------------------------------------------------------------------------------------------
+    
     
     //add in map
     void addUser(string, User*);
 
     //
+    int(id) search(string object);
+    // use id to borrow
+    void borrow(id);
 
+    // use id to return 
+    void returnobj(id);
+
+    //resgister 
+    bool setID(string s); // make sure no same ID
+    void setPassWord(string s);
+
+    //login
+    bool logID(string ID, string Password);
 
 private:
     map<string, User*> _users;
-    map<string, object*> _objects;
+    map<string, vector<object*>> _objects;
     User*              _currentUser;
 
 
